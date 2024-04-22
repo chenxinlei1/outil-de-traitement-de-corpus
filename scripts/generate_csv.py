fcaba4f788b82266f8cc4e691e5cd689c6dacdd6
chenxinlei1 <1816557979@qq.com>
2024-04-22 18:26:55 +0200

    fix: modification de label

--- scripts/generate_csv.py
+++ scripts/generate_csv.py
@@ -1,23 +1,26 @@
-import csv
+import pandas as pd
 import os
 
 base_folder = './data/raw/'
-
-csv_file = './data/dog_breeds_images.csv' 
-
+csv_file = './data/dog_breeds_images.csv'
 csv_headers = ['id', 'image', 'label']
 
-with open(csv_file, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(csv_headers)  
+data = []
+image_id = 1
 
-    image_id = 1
+breed_folders = sorted([d for d in os.listdir(base_folder) if os.path.isdir(os.path.join(base_folder, d))])
 
-    for breed_folder in os.listdir(base_folder):
-        breed_path = os.path.join(base_folder, breed_folder)
-        if os.path.isdir(breed_path):
-            for image_file in os.listdir(breed_path):
-                if image_file.lower().endswith(('.png', '.jpg', '.jpeg')):
-                    image_path = os.path.join(breed_path, image_file)
-                    writer.writerow([image_id, image_path, breed_folder])
-                    image_id += 1
+breed_to_id = {breed: i for i, breed in enumerate(breed_folders)}
+
+for breed_folder in breed_folders:
+    breed_path = os.path.join(base_folder, breed_folder)
+    for image_file in os.listdir(breed_path):
+        if image_file.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image_path = os.path.join(breed_path, image_file)
+            breed_id = breed_to_id[breed_folder]
+            data.append([image_id, image_path, breed_id])
+            image_id += 1
+
+df = pd.DataFrame(data, columns=csv_headers)
+
+df.to_csv(csv_file, index=False)
